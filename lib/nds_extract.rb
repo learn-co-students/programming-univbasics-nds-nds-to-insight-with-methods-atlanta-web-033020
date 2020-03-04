cf04cebb06713df208e7c73ee6d241d7481932f8
Wendy Raven McNair <wen.mcnair@gmail.com>
2020-03-04 18:31:32 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,6 +5,17 @@ require 'directors_database'
 # using director_data as input
 def gross_for_director(director_data)
 
+  # Refactor into gross_for_director
+          dire_gross = 0
+          director_x = 0
+          while director_x < director_data[:movies].length do
+            dire_gross += director_data[:movies][director_x][:worldwide_gross]
+                        # Why don't I need a row parameter to dig into this NDS
+            director_x += 1
+          end
+  # End of refactor gross_for_director
+
+          dire_gross
 end
 
 # Write a method that, given an NDS creates a new Hash
@@ -13,5 +24,17 @@ end
 # { directorOne => allTheMoneyTheyMade, ... }
 def directors_totals(nds)
   result = {}
+          row = 0
+          while row < nds.length do
+            total = 0
+            inner_len = nds[row]
+
+            total +=  gross_for_director(inner_len)
+            result[inner_len[:name]] = total
+
+            row += 1
+          end
+
   nil
+  result
 end
